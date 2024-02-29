e7c276f13ccc1df062a54e63e49f2efea863df4d
Flopy <the22thoctober@gmail.com>
2024-02-29 17:56:28 +0200

    auth added

--- client/app/Provider.tsx
+++ client/app/Provider.tsx
@@ -3,18 +3,21 @@ import React, { useState } from "react"
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental"
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { SessionProvider } from "next-auth/react"
 
 function Provider({ children }: any) {
     const [client] = useState(new QueryClient())
 
     return (
         <>
-            <QueryClientProvider client={client}>
-                <ReactQueryStreamedHydration>
-                    {children}
-                </ReactQueryStreamedHydration>
-                <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
+            <SessionProvider>
+                <QueryClientProvider client={client}>
+                    <ReactQueryStreamedHydration>
+                        {children}
+                    </ReactQueryStreamedHydration>
+                    <ReactQueryDevtools initialIsOpen={false} />
+                </QueryClientProvider>
+            </SessionProvider>
         </>
     )
 }

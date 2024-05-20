f2a029f2a0985202b78f9b31dab60e309a18ac08
the22ndoctober <the22thoctober@gmail.com>
2024-05-20 12:35:10 +0300

    123

--- client/app/components/header/search/Search.tsx
+++ client/app/components/header/search/Search.tsx
@@ -40,7 +40,7 @@ const Search = ({ params, session }: any) => {
     }
 
     useEffect(() => {
-        setCartAmount(cartList.length !== null ? cartList.length : 0)
+        setCartAmount(cartList !== null ? cartList.length : 0)
     }, [cartList])
 
     useEffect(() => {

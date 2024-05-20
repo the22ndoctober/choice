aacf57c681684861d8a0a6146ac9990c64c4e809
the22ndoctober <the22thoctober@gmail.com>
2024-05-20 10:06:18 +0300

    loginless

--- client/app/register/page.jsx
+++ client/app/register/page.jsx
@@ -1,7 +1,6 @@
 import RegisterForm from "@/app/components/login/RegisterForm"
 import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
-import { authOptions } from "../api/auth/[...nextauth]/route"
 
 export default async function Register() {
     return <RegisterForm />

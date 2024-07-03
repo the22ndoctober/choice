7b4837c5297abec6c915d023508721ceedf97d4d
Floppe <the22thoctober@gmail.com>
2024-07-03 23:58:20 +0300

    save the world

---
+++ client/app/components/saveTheWorld/SaveTheWorld.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { Box, Grid } from "@mui/material"
+
+//Кольори
+import { Colors } from "@/client"
+
+const SaveTheWorld = ({}) => {
+    return (
+        <Box
+            sx={{ width: "100%", minHeight: "70svh", background: Colors.paper }}
+        >
+            <Grid
+                container
+                sx={{
+                    margin: "0 auto",
+                    flexDirection: "column",
+                    width: { xl: 1440, lg: 1368, xs: 360 },
+                }}
+            >
+                {
+                    //start code here
+                }
+            </Grid>
+        </Box>
+    )
+}
+
+export default SaveTheWorld
--- client/app/savetheworld/page.tsx
+++ client/app/savetheworld/page.tsx
@@ -1,7 +1,17 @@
 import React from "react"
+import Footer from "@/app/components/footer/Footer"
+import SaveTheWorld from "@/app/components/saveTheWorld/SaveTheWorld"
+import Navigtaion from "@/app/components/header/navgitation/Navigation"
+import Search from "@/app/components/header/search/Search"
 
-const SaveTheWorld = () => {
-    return <div>SaveTheWorld</div>
+const SaveTheWorldPage = () => {
+    return (
+        <>
+            <Navigtaion />
+            <Search />
+            <SaveTheWorld />
+        </>
+    )
 }
 
-export default SaveTheWorld
+export default SaveTheWorldPage

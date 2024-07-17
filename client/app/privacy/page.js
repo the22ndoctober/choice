4a876b6b4396852378dd2f3ac5394e83a0ae0b94
the22ndoctober <the22thoctober@gmail.com>
2024-07-17 18:05:44 +0300

    privacy added

---
+++ client/app/privacy/page.js
@@ -0,0 +1,14 @@
+import React from "react"
+import OrderNav from "../components/order/OrderNav"
+import Privacy from "@/app/components/privacy/Privacy"
+
+const PrivacyPage = () => {
+    return (
+        <>
+            <OrderNav />
+            <Privacy />
+        </>
+    )
+}
+
+export default PrivacyPage
---
+++ client/app/publicOffers/page.js
@@ -0,0 +1,14 @@
+import React from "react"
+import OrderNav from "../components/order/OrderNav"
+import PublicOffers from "@/app/components/privacy/PublicOffers"
+
+const PublicOffersPage = () => {
+    return (
+        <>
+            <OrderNav />
+            <PublicOffers />
+        </>
+    )
+}
+
+export default PublicOffersPage
